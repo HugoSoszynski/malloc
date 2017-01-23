84c140a4cbb2085f2e6ba1e1933692762993c6ae
gregoire.loens_g <gregoire.loens@epitech.eu>
2017-01-23 10:40:34 +0100

    "initialisation du repo"

---
+++ my_malloc.c
@@ -0,0 +1,12 @@
+/*
+** my_malloc.c for PSU_2016_malloc in /home/loens_g/rendu/TEK2/SystemUnix/PSU_2016_malloc/my_malloc.c
+**
+** Made by Gregoire LOENS
+** Login   <gregoire.loens@epitech.eu>
+**
+** Started on  Mon Jan 23 09:34:22 2017 Gregoire LOENS
+** Last update Mon Jan 23 09:34:22 2017 Gregoire LOENS
+*/
+
+#include "my_malloc.h"
+
---
+++ my_malloc.h
@@ -0,0 +1,16 @@
+/*
+** my_malloc.h for PSU_2016_malloc in /home/loens_g/rendu/TEK2/SystemUnix/PSU_2016_malloc/my_malloc.h
+**
+** Made by Gregoire LOENS
+** Login   <gregoire.loens@epitech.eu>
+**
+** Started on  Mon Jan 23 09:35:04 2017 Gregoire LOENS
+** Last update Mon Jan 23 09:35:06 2017 Gregoire LOENS
+*/
+
+#ifndef MY_MALLOC_H
+# define MY_MALLOC_H
+
+
+
+#endif /* !PSU_2016_MALLOC_MY_MALLOC_H */
