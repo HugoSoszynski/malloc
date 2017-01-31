8bd8e356d7964c2e14e2d337e2d0a2e9d3c9df42
gregoire.loens_g <gregoire.loens@epitech.eu>
2017-01-31 11:00:07 +0100

    calloc done

---
+++ my_calloc.c
@@ -0,0 +1,25 @@
+/*
+** calloc.c for malloc in /home/loens_g/rendu/TEK2/malloc/calloc.c
+**
+** Made by Gregoire LOENS
+** Login   <gregoire.loens@epitech.eu>
+**
+** Started on  Tue Jan 31 09:45:03 2017 Gregoire LOENS
+** Last update Tue Jan 31 09:45:03 2017 Gregoire LOENS
+*/
+
+#include 	<string.h>
+#include	"my_malloc.h"
+
+void		*calloc(size_t nmemb, size_t size)
+{
+  void		*space;
+
+  if (nmemb == 0)
+    return (NULL);
+  space = malloc(size);
+  if (space == NULL)
+    return (NULL);
+  memset(space, 0, nmemb);
+  return (space);
+}
